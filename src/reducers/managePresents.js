99b9cbb7722ced07fd6cc397db9628382af7f211
breannams <breannamarie_s@hotmail.com>
2021-09-09 22:54:05 -0400

    commit tree 18b12dcb38357a6c9d57999bcdd6fbb8480b64f6

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
+let numberOfPresents = {count: 0}
+let action = {type:"INCREASE"}
+
 export function managePresents(state, action){
+    switch (action.type){
+        case "INCREASE":
+        return {numberOfPresents: state.numberOfPresents + 1}
+        default:
+            return state
+    }
+
 }
